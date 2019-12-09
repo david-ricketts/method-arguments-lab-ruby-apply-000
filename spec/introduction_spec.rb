00a294c0ff6e375cd1b441d6d0094c6f56aba1ce
David Ricketts <david.ricketts119@gmail.com>
2019-12-09 14:13:54 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -11,3 +11,7 @@ describe "#introduction_with_language" do
     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
   end
 end
+
+
+introduction("Josh")
+introduction_with_language("Dan", "Ember.js")
